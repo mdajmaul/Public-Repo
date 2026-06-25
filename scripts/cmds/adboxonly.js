11d9e762d8db04869632192c3c17b3ff337b9e4f
mdajmaul <mdajmaulislam00@gmail.com>
2026-06-25 21:50:13 +0600

    Fix adboxonly.js: replace ALVI-BOSS with Ajmaul

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -3,7 +3,7 @@ module.exports = {
 		name: "onlyadminbox",
 		aliases: ["onlyadbox", "adboxonly", "adminboxonly"],
 		version: "1.3",
-		author: "ALVI-BOSS",
+		author: "Ajmaul",
 		countDown: 5,
 		role: 1,
 		description: {
