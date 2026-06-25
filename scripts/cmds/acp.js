10b2f47f8e7218042f5fcf1369cb5b10fb5e85e5
mdajmaul <mdajmaulislam00@gmail.com>
2026-06-25 21:50:10 +0600

    Fix acp.js: replace ALVI-BOSS with Ajmaul

--- scripts/cmds/acp.js
+++ scripts/cmds/acp.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "accept",
     aliases: ['acp'],
     version: "1.0",
-    author: "ALVI-BOSS",
+    author: "Ajmaul",
     countDown: 8,
     role: 2,
     shortDescription: "accept users",
