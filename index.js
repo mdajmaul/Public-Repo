c0a431fe3e44f5e9bbf2c7b4392a9d9c2ec7c267
mdajmaul <mdajmaulislam00@gmail.com>
2026-06-25 20:37:17 +0600

    Fix: Replace hardcoded port 3000 with process.env.PORT || 3000

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.get('/', (req, res) => {
   res.send('Bot is running!');
 });
 
-app.listen(3000, () => {
-  console.log('Uptime server running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Uptime server running on port ${process.env.PORT || 3000}`);
 });
